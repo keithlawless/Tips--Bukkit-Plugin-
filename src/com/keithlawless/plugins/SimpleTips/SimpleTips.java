e740f6a923a32722156bff2245945c4392323368
Keith Lawless <keithlawless@gmail.com>
2011-05-23 22:37:11 -0400

    V 0.2 Added text color support.

--- src/com/keithlawless/plugins/SimpleTips/SimpleTips.java
+++ src/com/keithlawless/plugins/SimpleTips/SimpleTips.java
@@ -18,17 +18,24 @@
 
 package com.keithlawless.plugins.SimpleTips;
 
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.util.config.Configuration;
 
 import java.io.File;
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 import java.util.logging.Logger;
 
 public class SimpleTips extends JavaPlugin implements Runnable {
-    private static String version = "SimpleTips v0.2 by keithlawless";
+    private static String version = "SimpleTips v0.3 by keithlawless";
     Logger log = Logger.getLogger("Minecraft");
 
 
@@ -38,7 +45,9 @@ public class SimpleTips extends JavaPlugin implements Runnable {
     private List<String> msgs;
     private int msgCount = 0;
     private int currentMsg = 0;
+    private Random random = new Random();
 
+    public static PermissionHandler permissionHandler;
 
     public void onDisable() {
         log.info(version+" has been disabled.");
@@ -55,6 +64,8 @@ public class SimpleTips extends JavaPlugin implements Runnable {
         else {
             log.info(version+" Success! SimpleTips will be displayed on your schedule.");
         }
+
+        setupPermissions();
     }
 
     public void load() {
@@ -109,6 +120,55 @@ public class SimpleTips extends JavaPlugin implements Runnable {
                 currentMsg = 0;
             }
         }
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if(!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player)sender;
+
+        if(command.getName().equalsIgnoreCase("tip")) {
+
+            // player entered just /tip by itself - return a random tip
+            if( args.length == 0 ) {
+                int x = random.nextInt( msgCount );
+                player.sendMessage( escape_colors(msgs.get(x)));
+                return true;
+            }
+
+            if( args.length >= 1 ) {
+                // player entered /tip list
+                if( args[0].equalsIgnoreCase("list")) {
+
+                    if (!SimpleTips.permissionHandler.has(player, "tip.list")) {
+                        player.sendMessage( "(SimpleTips) You don't have permission to run that command.");
+                    }
+                    else {
+                        for( int x = 0; x < msgCount; x++ ) {
+                            player.sendMessage( "(" + x + ") " + escape_colors(msgs.get(x)));
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void setupPermissions() {
+      Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+      if (this.permissionHandler == null) {
+          if (permissionsPlugin != null) {
+              this.permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+          } else {
+              log.info("Permission system not detected, defaulting to OP");
+          }
+      }
     }
 
     private String escape_colors(String input) {
